547e7723c10bdc1d4ebc820b96dd45edcf01e406
smahale <16042612+nathmahale@users.noreply.github.com>
2021-02-20 08:30:23 +1100

    changing build_dir ref

--- temp-scripts/run-integration-tests.sh
+++ temp-scripts/run-integration-tests.sh
@@ -2,29 +2,27 @@
 
 OLD_APP_PROCESS=$(ps -ef | grep DevOpsChallenge.SalesApi.dll | grep -v grep | cut -d' ' -f2)
 
-if [ $? -eq 0 ];
+if [ -z "$OLD_APP_PROCESS" ];
     then
         echo "[ INFO ] killing old server process"
-        sleep 30
+        sleep 20
         kill -9 $OLD_APP_PROCESS
 else
     echo "[ ERROR ] Please check logs"
-    exit 1
 fi
 
-dotnet $BUILD_DIR/out/DevOpsChallenge.SalesApi.dll &
+dotnet $BUILD_DIR/src/DevOpsChallenge.SalesApi/bin/Release/net5.0/DevOpsChallenge.SalesApi.dll &
 APP_RUN_PROCESS_ID=$!
 
 echo $APP_RUN_PROCESS_ID
 dotnet test --verbosity minimal $BUILD_DIR/tests/DevOpsChallenge.SalesApi.IntegrationTests
 
-if [ $? -eq 0 ];
+if [ -z "$APP_RUN_PROCESS_ID" ];
     then
         echo "[ INFO ] dotnet integration testing is completed successfully!"
-        sleep 30
+        sleep 20
         kill -9 $APP_RUN_PROCESS_ID
         exit 0
 else
     echo "[ ERROR ] Please check the logs"
-    exit 1
 fi
